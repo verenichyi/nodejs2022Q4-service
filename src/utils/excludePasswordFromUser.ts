871e63b7543d1b17f4fa503f3ad91c67a418edea
verenichyi <verenichyi@gmail.com>
2023-02-02 16:09:24 +0300

    feat: implement /artist route

--- src/utils/excludePasswordFromUser.ts
+++ src/utils/excludePasswordFromUser.ts
@@ -1,7 +1,7 @@
 import User from '../users/interfaces/user.interface';
-import { Track } from '../users/types/return-user.type';
+import { ReturnUser } from '../users/types/return-user.type';
 
-export const excludePasswordFromUser = (user: User): Track => {
+export const excludePasswordFromUser = (user: User): ReturnUser => {
   const { id, login, version, createdAt, updatedAt } = user;
   return { id, login, version, createdAt, updatedAt };
 };
